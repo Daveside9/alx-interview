c9a1d852f1c53309f85be01a5aabffe721086bd8
Joel David <106955759+Daveside9@users.noreply.github.com>
2023-04-25 20:25:03 +0300

    Update main_0.py

--- 0x01-lockboxes/main_0.py
+++ 0x01-lockboxes/main_0.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 
-# Import the canUnlockAll function from the module 0-lockboxes
 canUnlockAll = __import__('0-lockboxes').canUnlockAll
 
-# Test case 1
 boxes = [[1], [2], [3], [4], []]
 print(canUnlockAll(boxes))
 
-# Test case 2
 boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
 print(canUnlockAll(boxes))
 
-# Test case 3
 boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
 print(canUnlockAll(boxes))
