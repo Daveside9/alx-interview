fec4fd642e78e322519fe39f6ee95da12f3b765d
Joel David <106955759+Daveside9@users.noreply.github.com>
2023-05-10 15:38:09 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,52 +1,36 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""reads stdin line by line and computes metrics:"""
 
 import sys
 
-TOTAL_LINES_TO_READ = 10
-
-# Status codes to track
-status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
-
-# Dictionary to store the number of lines by status code
-num_lines_by_status = {}
-for code in status_codes:
-    num_lines_by_status[code] = 0
-
-# Total file size
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
 total_size = 0
-
-# Counter to keep track of the number of lines read
-line_count = 0
+counter = 0
 
 try:
     for line in sys.stdin:
-        # Parse the line
-        try:
-            parts = line.split()
-            ip_address = parts[0]
-            date = parts[3][1:]
-            status_code = int(parts[8])
-            file_size = int(parts[9])
-        except:
-            # Skip this line if it doesn't match the expected format
-            continue
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
+            counter += 1
 
-        # Update metrics
-        total_size += file_size
-        num_lines_by_status[status_code] += 1
-        line_count += 1
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
 
-        # Print stats every TOTAL_LINES_TO_READ lines
-        if line_count % TOTAL_LINES_TO_READ == 0:
-            print("Total file size: {}".format(total_size))
-            for code in sorted(num_lines_by_status.keys()):
-                if num_lines_by_status[code] > 0:
-                    print("{}: {}".format(code, num_lines_by_status[code]))
+except Exception as err:
+    pass
 
-except KeyboardInterrupt:
-    # Print final stats on keyboard interrupt
-    print("Total file size: {}".format(total_size))
-    for code in sorted(num_lines_by_status.keys()):
-        if num_lines_by_status[code] > 0:
-            print("{}: {}".format(code, num_lines_by_status[code]))
-
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
