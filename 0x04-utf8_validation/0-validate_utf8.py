086b9c9181e501fa61523f5af962ea27d23b4d36
Joel David <106955759+Daveside9@users.noreply.github.com>
2023-05-18 11:33:04 -0700

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,27 +1,42 @@
+#!/usr/bin/python3
+"""
+UTF-8 Validation
+"""
+
+
 def validUTF8(data):
-    """
-    Determines if a given data set represents a valid UTF-8 encoding.
+    # Number of bytes in the current UTF-8 character
+    num_bytes = 0
 
-    Args:
-        data (list): A list of integers representing bytes of data.
-
-    Returns:
-        bool: True if data is a valid UTF-8 encoding, False otherwise.
-    """
-    num_bytes_to_check = 0
+    # Iterate over each integer in the data set
     for num in data:
-        if num_bytes_to_check == 0:
-            if (num >> 5) == 0b110:
-                num_bytes_to_check = 1
-            elif (num >> 4) == 0b1110:
-                num_bytes_to_check = 2
-            elif (num >> 3) == 0b11110:
-                num_bytes_to_check = 3
-            elif (num >> 7):
-                return False
-        else:
-            if (num >> 6) != 0b10:
-                return False
-            num_bytes_to_check -= 1
-    return num_bytes_to_check == 0
+        # Get the 8 least significant bits of the integer
+        binary = format(num, '08b')
 
+        # If it's the start of a new character
+        if num_bytes == 0:
+            # Count the number of leading 1s to determine the number of bytes
+            for bit in binary:
+                if bit == '0':
+                    break
+                num_bytes += 1
+
+            # Handle invalid number of bytes
+            if num_bytes == 0:
+                continue
+
+            '''For characters with a single byte, no
+            need to check continuation bytes'''
+            if num_bytes == 1 or num_bytes > 4:
+                return False
+
+        else:
+            # Check if the current byte is a valid continuation byte
+            if not (binary[0] == '1' and binary[1] == '0'):
+                return False
+
+        # Decrement the count of remaining bytes
+        num_bytes -= 1
+
+    # All bytes have been validated
+    return num_bytes == 0
