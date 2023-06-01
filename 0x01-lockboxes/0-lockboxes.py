9b18e8c47c0665cbb6d005d94e75c9ec67d4b14e
joeldavid <daveside00468@gmail.com>
2023-06-01 16:10:32 -0700

    Test

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,7 +6,6 @@ Each box is numbered sequentially from 0 to n - 1 and each box may
 contain keys to the other boxes.
 """
 
-
 def canUnlockAll(boxes):
     """
     Determine if all the boxes can be opened.
