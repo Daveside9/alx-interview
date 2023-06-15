5ed33e0ff18a0cfadfc23c8bf9942f895d11cbf3
Joel David <106955759+Daveside9@users.noreply.github.com>
2023-06-15 03:02:54 -0700

    Update main.py

--- 0x08-making_change/main.py
+++ 0x08-making_change/main.py
@@ -1,2 +1,10 @@
-Minimum number of coins needed for total 37: 7
-Minimum number of coins needed for total 1453: -1
+#!/usr/bin/python3
+"""
+Main file for testing
+"""
+
+makeChange = __import__('0-making_change').makeChange
+
+print(makeChange([1, 2, 25], 37))
+
+print(makeChange([1256, 54, 48, 16, 102], 1453))
